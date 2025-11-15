8219337a19914d3ded099cd0ea93409cb5ce937f
kurbik72 <jumperchannel79@gmail.com>
2025-11-15 22:35:28 +0300

    Refactor HTTP service: Replace axios with a custom httpService function that supports mock responses and delays for testing. Introduce interfaces for mock options and request options to enhance flexibility in API calls.

--- frontend/src/shared/service/http-service.ts
+++ frontend/src/shared/service/http-service.ts
@@ -1,8 +1,37 @@
-import axios from 'axios'
+import { useFetch, type UseFetchOptions } from '@vueuse/core'
 
-export const apiService = axios.create({
-  baseURL: import.meta.env.HTTP_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
+interface MockError {
+  errorMessage: string
+  statusCode: number
+}
+
+interface MockOptions<DataType> {
+  data: DataType
+  isError?: boolean
+  error?: MockError
+  delay?: number
+  enabled?: boolean
+}
+
+interface RequestOptions<DataType> extends UseFetchOptions {
+  mock: MockOptions<DataType>
+}
+
+export const httpService = async <DataType>(url: string, options: RequestOptions<DataType>) => {
+  if (options.mock.enabled) {
+    const mockDelay = options.mock.delay || 1000
+
+    await new Promise((resolve) => setTimeout(resolve, mockDelay))
+
+    if (options.mock.error) {
+      throw {
+        errorMessage: options.mock.error?.errorMessage || 'Internal Server Error',
+        statusCode: options.mock.error?.statusCode || 500,
+      } as MockError
+    }
+
+    return options.mock.data
+  }
+
+  return useFetch(url, options)
+}
