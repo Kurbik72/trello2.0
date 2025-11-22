e9e188b2730c5ffdd028bae1d33899a2da5c9973
kurbik72 <jumperchannel79@gmail.com>
2025-11-22 22:22:37 +0300

    Refactor boards store to include API integration: Update Dashboard interface to DashboardResponse, add DashboardRequest interface, and implement saveBoard function for creating new boards via HTTP service.

--- frontend/src/shared/stores/boards.ts
+++ frontend/src/shared/stores/boards.ts
@@ -1,16 +1,26 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { httpService } from '../service/http-service'
+import { useFetch } from '@vueuse/core'
 
-interface Dashboard {
+interface DashboardResponse {
   id: string
   title: string
   isFavorite: boolean
   linkToBoard: string
-  user_id: string
 }
 
+interface DashboardRequest {
+  user_id: string
+}
 export const useBoardsStore = defineStore('boards', () => {
-  const boards = ref<Dashboard[]>([])
+  const boards = ref<DashboardResponse[]>([])
 
+  const saveBoard = async (board: DashboardRequest) => {
+    const { data: boardResponse } = await httpService.post<DashboardResponse[]>('/api/boards', {
+      body: JSON.stringify(board),
+    })
+    boards.value = boardResponse.value ?? []
+  }
   return { boards }
 })
