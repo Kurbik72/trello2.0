2da060a6bb1faafc6ed1f6595853f2c2362c46d7
kurbik72 <jumperchannel79@gmail.com>
2025-12-20 18:30:25 +0300

    Refactor icon component: Update icon handling to support optional primeVue icons, improve size management with computed properties, and enhance template structure for better attribute handling.

--- frontend/src/shared/ui-kit/icon/icon.vue
+++ frontend/src/shared/ui-kit/icon/icon.vue
@@ -1,32 +1,36 @@
 <script setup lang="ts">
 import { iconIds } from '@/shared/constants/iconIds'
-import { defineAsyncComponent, shallowRef, toRefs, watchEffect } from 'vue'
+import { computed, defineAsyncComponent, useAttrs } from 'vue'
 
 interface IconProps {
   width: number
   height: number
-  iconId: iconIds
+  iconId?: iconIds
+  primeVueIcon?: string
 }
 
 const props = defineProps<IconProps>()
 
-const { width, height } = toRefs(props)
-
-const iconPath = shallowRef()
-
-watchEffect(() => {
-  iconPath.value = defineAsyncComponent(() => import(`@/assets/icons/${props.iconId}.svg`))
-})
+const sizeOfIcon = computed(() => ({
+  width: `${props.width}px`,
+  height: `${props.height}px`,
+}))
+const icon = computed(() =>
+  props.iconId ? defineAsyncComponent(() => import(`@/assets/icons/${props.iconId}.svg`)) : 'i',
+)
+const attrs = useAttrs()
+// TODO: add map for primevue icons for view like this: <i name="user" />
+const styleIcon = computed(() => ['icon', `${props.primeVueIcon}`])
 </script>
 
 <template>
-  <component :is="iconPath" class="icon" />
+  <component :is="icon" :class="styleIcon" :attrs="attrs" />
 </template>
 
 <style scoped>
 .icon {
-  width: v-bind('width + "px"');
-  height: v-bind('height + "px"');
+  width: v-bind('sizeOfIcon.width');
+  height: v-bind('sizeOfIcon.height');
   overflow: hidden;
 }
 </style>
